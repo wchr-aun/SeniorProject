03c7aa08c88389c3c652c52096cb39787968d835
anaunz <anaunz@gmail.com>
2019-12-30 08:15:31 +0700

    modifier getTx()

--- Application/store/actions/transactionAction.js
+++ Application/store/actions/transactionAction.js
@@ -5,7 +5,11 @@ export const FETCH_TRANSACTION = "FETCH_TRANSACTION";
 
 export const fetchTransaction = () => {
   return async dispatch => {
-    let transactions = await firebaseFunctions.getTransactions("seller", 0);
+    let transactions = await firebaseFunctions.getTransactions("seller");
+    
+    /***************************CHANGE THIS********************************/
+    transactions = transactions[0] // Change this to whatever you need to
+    /**********************************************************************/
 
     // Get 'amountOfType' and 'assignTime' in the right format
     transactions.forEach((item, index) => {
