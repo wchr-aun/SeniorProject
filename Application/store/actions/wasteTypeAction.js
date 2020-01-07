8a41db2482e16f4ba6c14515bf6371cda8d95128
Norasecure <elesrubsoap_4822@hotmail.com>
2020-01-08 03:43:54 +0700

    Know 'How to do sectionList' with wasteType firestore

--- Application/store/actions/wasteTypeAction.js
+++ Application/store/actions/wasteTypeAction.js
@@ -4,8 +4,10 @@ export const FETCH_WASTETYPE = "FETCH_WASTETYPE";
 
 export const fetchWasteType = () => {
   return async dispatch => {
-    let wasteTypes = [];
-    wasteTypes = await getWasteType();
-    dispatch({ type: FETCH_WASTETYPE, wasteTypes });
+    let wasteTypesList = [];
+    wasteTypesList = await getWasteType();
+    // console.log("wasteTypes --- fetchWasteType --- wasteTypeAction");
+    // console.log(wasteTypesList);
+    dispatch({ type: FETCH_WASTETYPE, wasteTypesList });
   };
 };
