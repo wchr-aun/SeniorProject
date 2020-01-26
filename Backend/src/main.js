67d780437678c7327ce2e763c7f19512cee9bfff
titlethanason <titlethanason@gmail.com>
2020-01-27 01:27:52 +0700

    add uploadImage

--- Backend/src/main.js
+++ Backend/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ImageUploader from 'vue-image-upload-resize'
+import ProgressBar from 'vuejs-progress-bar'
+import VueResource from "vue-resource"
 
+Vue.use(VueResource);
+Vue.use(ProgressBar)
+Vue.use(ImageUploader);
 Vue.config.productionTip = false
 
 new Vue({
--- Backend/src/router/index.js
+++ Backend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home'
 import Annotation from '../views/Annotation'
+import uploadImage from '../views/uploadImage'
 
 Vue.use(VueRouter)
 
@@ -25,6 +26,11 @@ const routes = [
     path: '/annotate',
     name: 'annotate',
     component: Annotation
+  },
+  {
+    path: '/uploadImage',
+    name: 'uploadImage',
+    component: uploadImage
   }
 ]
 
