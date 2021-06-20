b16e2df2e1fdef7a60deb3ec30bb474793bd68f0
wchr-aun <wchr.aun@gmail.com>
2021-06-20 21:40:41 +0700

    :pencil2: (constants/routes) to fix typo of protected routes

--- src/lib/guards/index.ts
+++ src/lib/guards/index.ts
@@ -1,9 +1,9 @@
-import { PROTECTE_ROUTES, ROUTES } from '$lib/constants/routes';
+import { PROTECTED_ROUTES, ROUTES } from '$lib/constants/routes';
 import { isLogin$ } from '$lib/store';
 import type { LoadInput, LoadOutput } from '@sveltejs/kit';
 
 let isLogin = false;
-const protectedPath = PROTECTE_ROUTES;
+const protectedPath = PROTECTED_ROUTES;
 isLogin$.subscribe((authState) => (isLogin = authState));
 
 export async function authGuard({ page }: LoadInput): Promise<LoadOutput> {
